5ea9b5dc90ae0aefd6c06e3ed41df50d4c284abb
bhavya654 <bhavyaksingh654@gmail.com>
2026-04-30 15:51:35 +0530

    Backend Setup

---
+++ bms-backend/src/config/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { config } from "./config";
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(config.databaseURL as string);
+        console.log("Connected to MongoDB");
+    } 
+    catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+export default connectDB;
